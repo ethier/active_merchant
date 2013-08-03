530f93bf81e4ea42b5209edbd430980f431c7c20
Derek Ethier <derek.ethier@gmail.com>
2013-08-03 11:14:40 -0400

    Add ReturnFMFDetails node to the sale/authorization request for Paypal if the option is enabled
    The return_fmf_details option was not available in the build_sale_or_authorization_request method so FMF responses were not being returned for DoDirectPayment requests even with that option enabled.

--- lib/active_merchant/billing/gateways/paypal.rb
+++ lib/active_merchant/billing/gateways/paypal.rb
@@ -55,6 +55,7 @@ module ActiveMerchant #:nodoc:
               add_credit_card(xml, credit_card_or_referenced_id, billing_address, options) unless transaction_type == 'DoReferenceTransaction'
               xml.tag! 'n2:IPAddress', options[:ip]
             end
+            xml.tag! 'ReturnFMFDetails', options[:return_fmf_details]
           end
         end
 
